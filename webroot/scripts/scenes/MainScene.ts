8c2f975db1c691ded04d2f4f02e40efbf29633b0
hrecker <henryjrecker@gmail.com>
2023-06-26 20:40:25 -0500

    Basic word building with the tiles

--- webroot/scripts/scenes/MainScene.ts
+++ webroot/scripts/scenes/MainScene.ts
@@ -7,8 +7,13 @@ export class MainScene extends Phaser.Scene {
     boardTiles: Phaser.GameObjects.Sprite[][];
     boardLetters: Phaser.GameObjects.Text[][];
     resetButton: Phaser.GameObjects.Sprite;
+    finishedWord: Phaser.GameObjects.Text;
 
     selectedButton: string;
+    buildingWord: boolean;
+    currentWord: string[];
+    tilesUsed: boolean[][];
+    lastTile: Phaser.Math.Vector2;
 
     constructor() {
         super({
@@ -26,21 +31,29 @@ export class MainScene extends Phaser.Scene {
 
     create() {
         this.cameras.main.setBackgroundColor(0x963C3C);
+        this.buildingWord = false;
+        this.currentWord = [];
+        this.lastTile = Phaser.Math.Vector2.ZERO.clone();
 
+        this.tilesUsed = [];
         this.boardLetters = [];
         this.boardTiles = [];
         for (let i = 0; i < config()["boardSize"]; i++) {
             this.boardLetters.push([]);
             this.boardTiles.push([]);
+            this.tilesUsed.push([]);
             for (let j = 0; j < config()["boardSize"]; j++) {
-                //TODO resizing
-                this.boardTiles[i].push(this.add.sprite((j + 1) * tileMargin + 200, (i + 1) * tileMargin - 10, "tile"));
-                this.boardLetters[i].push(this.add.text((j + 1) * tileMargin + 200, (i + 1) * tileMargin - 10, "", config()["tileStyle"]).setOrigin(0.5));
+                this.createTile(i, j);
+                this.createTileText(i, j);
+                this.tilesUsed[i].push(false);
             }
         }
 
         this.resetButton = this.add.sprite(80, 200, "tile");
         this.configureButton(this.resetButton, "reset");
+
+        // Track a few finished words
+        this.finishedWord = this.add.text(520, 190, "", { ...config()["tileStyle"], font: "bold 26px Verdana" });
 
         this.loadBoard(generateBoard(config()["boardSize"]));
 
@@ -81,6 +94,76 @@ export class MainScene extends Phaser.Scene {
             case "reset":
                 this.loadBoard(generateBoard(config()["boardSize"]));
                 break;
+        }
+    }
+
+    createTile(i: number, j: number) {
+        //TODO resizing
+        let tile = this.add.sprite((j + 1) * tileMargin + 120, (i + 1) * tileMargin - 10, "tile");
+        tile.setData("index", new Phaser.Math.Vector2(i, j));
+        tile.setInteractive();
+        tile.on('pointerdown', () => {
+            this.startWord(i, j);
+        });
+        this.boardTiles[i].push(tile);
+    }
+
+    createTileText(i: number, j: number) {
+        //TODO resizing
+        let text = this.add.text((j + 1) * tileMargin + 120, (i + 1) * tileMargin - 10, "", config()["tileStyle"]).setOrigin(0.5);
+        text.setInteractive();
+        text.on('pointerdown', () => {
+            this.startWord(i, j);
+        });
+        text.on('pointerover', () => {
+            if (this.buildingWord && ! this.tilesUsed[i][j] && this.tileInRange(i, j)) {
+                this.addTileToWord(i, j);
+            }
+        });
+        this.boardLetters[i].push(text);
+    }
+
+    startWord(i: number, j: number) {
+        if (! this.buildingWord) {
+            console.log("Clicked tile: " + this.boardLetters[i][j].text);
+            this.buildingWord = true;
+            this.addTileToWord(i, j);
+        }
+    }
+
+    tileInRange(i: number, j: number) {
+        return Math.abs(i - this.lastTile.x) <= 1 && Math.abs(j - this.lastTile.y) <= 1;
+    }
+
+    addTileToWord(i: number, j: number) {
+        console.log("Adding text " + this.boardLetters[i][j].text);
+        this.currentWord.push(this.boardLetters[i][j].text);
+        this.tilesUsed[i][j] = true;
+        this.lastTile.x = i;
+        this.lastTile.y = j;
+        this.boardLetters[i][j].setColor("white");
+    }
+
+    finishWord() {
+        console.log("Finished word: " + this.currentWord);
+        for (let i = 0; i < this.tilesUsed.length; i++) {
+            for (let j = 0; j < this.tilesUsed[i].length; j++) {
+                this.tilesUsed[i][j] = false;
+                this.boardLetters[i][j].setColor("black");
+            }
+        }
+        let word = "";
+        this.currentWord.forEach(tile => {
+            word += tile;
+        })
+        this.finishedWord.text = word;
+        this.buildingWord = false;
+        this.currentWord = [];
+    }
+
+    update() {
+        if (this.buildingWord && ! this.input.activePointer.isDown) {
+            this.finishWord();
         }
     }
 }
